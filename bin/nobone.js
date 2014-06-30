c8c1f0b85cece9997844999e021ea8c8da7f3c7b
Yad Smood <y.s.inside@gmail.com>
2014-07-01 05:35:19 +0800

    opt: better for test

--- bin/nobone.js
+++ bin/nobone.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-require('../dist/cli');
+try	{
+	require('coffee-script/register');
+	require('../lib/cli');
+} catch (e) {
+	if (e.code === 'MODULE_NOT_FOUND')
+		require('../dist/cli');
+	else
+		throw e;
+}
