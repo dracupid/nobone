db717117bbfd3ccafad9034c54df7488576a3602
Yad Smood <y.s.inside@gmail.com>
2014-07-01 05:38:12 +0800

    Revert "opt: better for test"
    This reverts commit c8c1f0b85cece9997844999e021ea8c8da7f3c7b.

--- bin/nobone.js
+++ bin/nobone.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
 
-try	{
-	require('coffee-script/register');
-	require('../lib/cli');
-} catch (e) {
-	if (e.code === 'MODULE_NOT_FOUND')
-		require('../dist/cli');
-	else
-		throw e;
-}
+require('../dist/cli');
